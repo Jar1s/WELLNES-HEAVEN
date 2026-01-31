22c7f5b1c9a8c1a0c19e8029b12d0c78794b3cc4
Jaroslav <jaroslav@example.com>
2026-01-31 22:23:22 +0100

    Load GTM via direct script tag

--- wellness-families/app/layout.tsx
+++ wellness-families/app/layout.tsx
@@ -71,17 +71,17 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         {gtmId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `,
-            }}
-          />
+          <>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  window.dataLayer.push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                `,
+              }}
+            />
+            <script async src={`https://www.googletagmanager.com/gtm.js?id=${gtmId}`}></script>
+          </>
         )}
       </head>
       <body className={`${inter.variable} ${playfair.variable} font-body antialiased`}>
